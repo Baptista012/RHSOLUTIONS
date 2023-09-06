7d720378c61e8393d424f3188065da66fc27aa84
Matheusbaptist <matheusbaptista070@gmail.com>
2023-09-06 13:24:58 -0300

    subindo pela primeira vez

---
+++ RHSOLUTIONS/Models/FuncionarioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PIMIVRH.Models
+{
+    public class FuncionarioModel
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public string CargoFunc { get; set; }
+        public double SalarioFunc { get; set; }
+    }
+}
---
+++ RHSOLUTIONS/Models/PontoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIMIVRH.Models
+{
+    public class PontoModel
+    {
+        public DateTime PrimeiraBatida { get; set; }
+        public DateTime SegundaBatida { get; set; }
+        public DateTime TerceiraBatida { get; set; }
+        public DateTime QuartaBatida { get; set; }
+        public int HorasTrabalhadas { get; set; }
+        public string Cpf { get; set; }
+
+    }
+}
