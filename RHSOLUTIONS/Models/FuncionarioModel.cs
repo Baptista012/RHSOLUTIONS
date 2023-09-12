3a5fd1775d4d89152bb0a0fc3d3c41d4d3505c06
Matheusbaptist <matheusbaptista070@gmail.com>
2023-09-12 09:00:09 -0300

    atualização

--- RHSOLUTIONS/Models/FuncionarioModel.cs
+++ RHSOLUTIONS/Models/FuncionarioModel.cs
@@ -6,6 +6,8 @@
         public string Cpf { get; set; }
         public string Telefone { get; set; }
         public string CargoFunc { get; set; }
+        public string Email { get; set; }
         public double SalarioFunc { get; set; }
+        public ResidenciaModel residencia { get; set; }
     }
 }
